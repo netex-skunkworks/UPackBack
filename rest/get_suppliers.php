b988582afdf4064883c1699294212e9e00a8b1bf
samoletic <vpriguza@gmail.com>
2016-05-21 04:31:55 +0300

    changed rest/get_suppliers.php script to use updated getFromHBase()

--- rest/get_suppliers.php
+++ rest/get_suppliers.php
@@ -3,14 +3,6 @@
 require_once __DIR__ . '/config.php';
 require_once __DIR__ . '/hbase.php';
 
-
-$result = getFromHBase('/supplier/*');
-foreach ($result as $key => $supplier) {
-    if ( isset($supplier['address_id']) ) {
-        $addr = getFromHBase('/address/' . $supplier['address_id']);
-        $result[$key]['address'] = $addr[0];
-        unset($result[$key]['address_id']);
-    }
-}
+$result = getFromHBase('/supplier/*', ['address']);
 
 echo json_encode($result);
