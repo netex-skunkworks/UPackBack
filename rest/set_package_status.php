272cace5060ba474ca8e86b76c2221ef519b6a46
samoletic <vpriguza@gmail.com>
2016-05-21 14:56:46 +0300

    set package status

--- rest/set_package_status.php
+++ rest/set_package_status.php
@@ -3,9 +3,9 @@
 require_once __DIR__ . '/config.php';
 require_once __DIR__ . '/hbase.php';
 
-$packageId = $_GET['package_id'];
-$status = $_GET['status'];
-$courierId = null;
+$packageId = $_POST['package_id'];
+$status = strtoupper($_POST['status']);
+$courierId = 'null';
 
 if ($status == 'accepted') {
     $courierId = $_POST['courier_id'];
@@ -18,6 +18,7 @@ $columnName = 'p:status';
 $columnValue = $status;
 $apiCallPart = array();
 $apiCallPart[] = '{"key":"' . base64_encode($packageId) . '", "Cell": [{"column":"' . base64_encode($columnName) . '", "$":"' . base64_encode($columnValue) . '"}]} ';
+$apiCallPart[] = '{"key":"' . base64_encode($packageId) . '", "Cell": [{"column":"' . base64_encode('p:courier_id') . '", "$":"' . base64_encode($courierId) . '"}]} ';
 
 file_put_contents(__DIR__ . '/moveToHBASE', '{"Row":[' . implode(', ', $apiCallPart) . ']}');
 $apiCall .= " " . HBASE_API_URL . "/package/" . time();
