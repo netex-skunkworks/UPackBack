49aeddfbea5867895ddab115c9739dea6e41e491
samoletic <vpriguza@gmail.com>
2016-05-21 18:39:18 +0300

    implemented range filtering based on courier preferences and current position in get_suppliers.php script

--- rest/get_suppliers.php
+++ rest/get_suppliers.php
@@ -8,21 +8,28 @@ require_once __DIR__ . '/google_maps_api.php';
 $result = getFromHBase('/supplier/*', ['address']);
 
 $courierPosition = isset($_GET['position']) ? $_GET['position'] : false ;
-$maxDistance = isset($_GET['distance']) ? $_GET['distance'] : false ;
-$maxDuration = isset($_GET['duration']) ? $_GET['duration'] : false ;
 
-foreach ($result as $key => $res_value) {
-    if ( isset($result[$key]) && isset($courierPosition) && ( !empty($maxDistance) || !empty($maxDuration) ) ) {
+if ( isset($courierPosition) ) {
+    foreach ($result as $key => $res_value) {
+        $supplierId = $res_value['id'];
+        $prefs = getSupplierPreferences($supplierId);
         $checkRange = gapiCalculateDistance(
             explode(',', $courierPosition),
             [$res_value['address']['lat'], $res_value['address']['lng']]
         );
         if ( is_array($checkRange) ) {
-            checkAndFilterIfNotInRange($result, $key, getDistance($checkRange), $maxDistance);
-            checkAndFilterIfNotInRange($result, $key, getDuration($checkRange), $maxDuration);
+            checkAndFilterIfNotInRange($result, $key, getDistance($checkRange), $prefs['suppliers_distance']);
+            checkAndFilterIfNotInRange($result, $key, getDuration($checkRange), $prefs['suppliers_duration']);
         }
     }
 }
 
-
 echo json_encode($result);
+
+function getSupplierPreferences($supplierId) {
+    $result = getFromHBase('/preferences/'.$supplierId);
+    if ( is_array($result) && count($result) > 0 ) {
+        return $result[0];
+    }
+    return false;  //  no preferences
+}
