c6940e9b64027e0d935409e68dc94bfb77366512
samoletic <vpriguza@gmail.com>
2016-05-21 17:55:33 +0300

    added Google Maps API settings

--- rest/config.php
+++ rest/config.php
@@ -1,3 +1,10 @@
 <?php
 
 define('HBASE_API_URL', 'http://127.0.0.1:2626');
+
+define('GOOGLE_API_URL', 'https://maps.googleapis.com');
+define('GOOGLE_API_FUNC', 'maps/api/distancematrix');
+define('GOOGLE_API_FORMAT', 'json');
+define('GOOGLE_API_KEY', 'AIzaSyA0Qv-NGnRQSng4dhaTUi9LudyFNPZYjBY');
+
+?>
