7a8b9d713e78b4f2ac98b0bc5da6d33476badf41
samoletic <vpriguza@gmail.com>
2016-05-21 18:48:39 +0300

    minor fixes in get_suppliers.php script

--- rest/get_suppliers.php
+++ rest/get_suppliers.php
@@ -12,24 +12,18 @@ $courierPosition = isset($_GET['courier_id']) ? $_GET['courier_id'] : false ;
 
 if ( isset($courierId) && isset($courierPosition) ) {
     $prefs = getCourierPreferences($courierId);
-    foreach ($result as $key => $res_value) {
-        $checkRange = gapiCalculateDistance(
-            explode(',', $courierPosition),
-            [$res_value['address']['lat'], $res_value['address']['lng']]
-        );
-        if ( is_array($checkRange) ) {
-            checkAndFilterIfNotInRange($result, $key, getDistance($checkRange), $prefs['suppliers_distance']);
-            checkAndFilterIfNotInRange($result, $key, getDuration($checkRange), $prefs['suppliers_duration']);
+    if ( ! empty($prefs) ) {
+        foreach ($result as $key => $res_value) {
+            $checkRange = gapiCalculateDistance(
+                explode(',', $courierPosition),
+                [$res_value['address']['lat'], $res_value['address']['lng']]
+            );
+            if ( is_array($checkRange) ) {
+                checkAndFilterIfNotInRange($result, $key, getDistance($checkRange), $prefs['suppliers_distance']);
+                checkAndFilterIfNotInRange($result, $key, getDuration($checkRange), $prefs['suppliers_duration']);
+            }
         }
     }
 }
 
 echo json_encode($result);
-
-function getCourierPreferences($courierId) {
-    $result = getFromHBase('/preferences/'.$courierId);
-    if ( is_array($result) && count($result) > 0 ) {
-        return $result[0];
-    }
-    return false;  //  no preferences
-}
