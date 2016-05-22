bb78cbaf5e7a2aefeecda299232670671415a570
samoletic <vpriguza@gmail.com>
2016-05-22 03:13:57 +0300

    added address details for selected package

--- rest/get_package.php
+++ rest/get_package.php
@@ -5,6 +5,6 @@ require_once __DIR__ . '/hbase.php';
 
 $id = $_GET['id'];
 
-$result = getFromHBase('/package/' . $id);
+$result = getFromHBase('/package/' . $id, ['supplier', 'customer', 'address']);
 
 echo json_encode($result[0]);
