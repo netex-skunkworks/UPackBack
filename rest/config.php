740304e420c1c6ec8599905598932d6221f15ffc
samoletic <vpriguza@gmail.com>
2016-05-22 07:40:23 +0300

    api key

--- rest/config.php
+++ rest/config.php
@@ -5,6 +5,6 @@ define('HBASE_API_URL', 'http://127.0.0.1:2626');
 define('GOOGLE_API_URL', 'https://maps.googleapis.com');
 define('GOOGLE_API_FUNC', 'maps/api/distancematrix');
 define('GOOGLE_API_FORMAT', 'json');
-define('GOOGLE_API_KEY', 'AIzaSyA0Qv-NGnRQSng4dhaTUi9LudyFNPZYjBY');
+define('GOOGLE_API_KEY', 'AIzaSyCPWbbk_4nXzf-J2hxAIbg5wgxNDxAy-c8');
 
 ?>
